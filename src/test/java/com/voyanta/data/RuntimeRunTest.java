b7ee74aaccbbdc2661d777c18dae5f2e3c500ba2
sangajala <sriram.angajala@gmail.com>
2014-07-21 17:41:50 +0100

    sangajala completed export tests

---
+++ src/test/java/com/voyanta/data/RuntimeRunTest.java
@@ -0,0 +1,12 @@
+package com.voyanta.data;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by sriramangajala on 21/07/2014.
+ */
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = {"pretty", "html:target/cucumber","json:target/cucumber.json"})
+public class RuntimeRunTest {
+}
