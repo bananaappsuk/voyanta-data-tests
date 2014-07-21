d0b4c14afd3fa3e7fbcc1f7016ccb55d65650bd6
sangajala <sriram.angajala@gmail.com>
2014-07-21 21:03:24 +0100

    Update VoyantaCucumberTest.java

--- src/test/java/com/voyanta/data/VoyantaCucumberTest.java
+++ src/test/java/com/voyanta/data/VoyantaCucumberTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(tags="@done",format = {"pretty", "html:target/cucumber","json:target/cucumber.json"})
+@Cucumber.Options(tags="@done,@export",format = {"pretty", "html:target/cucumber","json:target/cucumber.json"})
 public class VoyantaCucumberTest {
 }
 
