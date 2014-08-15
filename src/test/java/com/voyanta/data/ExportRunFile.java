af1d62a0f02edd1d824116fe47096cad75e73e84
sangajala <sriram.angajala@gmail.com>
2014-08-15 14:32:24 +0100

    Update ExportRunFile.java

--- src/test/java/com/voyanta/data/ExportRunFile.java
+++ src/test/java/com/voyanta/data/ExportRunFile.java
@@ -7,6 +7,6 @@ import org.junit.runner.RunWith;
  * Created by sriramangajala on 14/07/2014.
  */
 @RunWith(Cucumber.class)
-@Cucumber.Options(tags={"@Taliance","~@ignore"},format = {"pretty", "html:target/cucumber","json:target/cucumber.json"})
+@Cucumber.Options(tags={"@export","~@ignore"},format = {"pretty", "html:target/cucumber","json:target/cucumber.json"})
 public class ExportRunFile {
 }
